c0e4f1b484570d4521dfbe79b85e4d153988ccf0
Declan Herbertson <dherbertson@alumni.ubc.ca>
2018-11-26 18:39:05 -0800

    allowing background remain fixed, content scroll

--- src/app/mode-toggle/mode-toggle.component.ts
+++ src/app/mode-toggle/mode-toggle.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {DarkModeService} from '../dark-mode.service';
 
+// todo add swipe gesture and make it larger on mobile
 @Component({
   selector: 'app-mode-toggle',
   templateUrl: './mode-toggle.component.html',
